6df5f8fdb73276040866e1f10fb32d019b3a17e5
Sanmerson Clay <san_merson3@hotmail.com>
2020-06-11 08:52:15 -0300

    Criação da classe req_snmp e ajustes no codigo do Cliente_SNMP

--- Cliente_SNMP/src/clientesnmp/ClienteSNMP.java
+++ Cliente_SNMP/src/clientesnmp/ClienteSNMP.java
@@ -1,13 +1,5 @@
 package clientesnmp;
 
-import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
-import org.snmp4j.Snmp;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.smi.*;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 public class ClienteSNMP {
 	 public static final String COMMUNITY = "public"; //comunidade
@@ -17,18 +9,20 @@ public class ClienteSNMP {
      public static void main(String[] args)
 
      {
-
+        int i=0;
+         while(i<1){
      try
 
      {
 
-     String strIPAddress = "172.0.0.1"; //Ip de destino para a requisição SNMP
+     String strIPAddress = "192.168.0.58"; //Ip de destino para a requisição SNMP
 
-     ClienteSNMP objSNMP = new ClienteSNMP(); // Inicia o Client
-
-     String teste =objSNMP.snmpGet(strIPAddress,COMMUNITY,OID_Temperatura, OID_Umidade, OID_Fumaca); // Chama o metodo de requisiçao
+    // ClienteSNMP objSNMP = new ClienteSNMP(); // Inicia o Client
+     Req_SNMP Requisicao_SNMP = new Req_SNMP();
+     String SNMP =Requisicao_SNMP.snmpGet(strIPAddress,COMMUNITY,OID_Temperatura, OID_Umidade, OID_Fumaca);
+    // String teste =objSNMP.snmpGet(strIPAddress,COMMUNITY,OID_Temperatura, OID_Umidade, OID_Fumaca); // Chama o metodo de requisiçao
      
-     System.out.print(teste);
+     System.out.print(SNMP);
      }
     
      catch (Exception e)
@@ -39,104 +33,6 @@ public class ClienteSNMP {
 
      }
 	
-     
-     
-
 }
-
-
-public String snmpGet(String strAddress, String community, String strOID, String strOID2, String strOID3)
-
-{
-	
-String str = "";
-String Temp="";
-String Umid = "";
-String Fum = "";
-try
-
-{
-
-OctetString community1 = new OctetString(community); //comunidade
-
-strAddress= strAddress+"/" + 161; // ip destino
-
-Address targetaddress = new UdpAddress(strAddress); 
-
-TransportMapping transport = new DefaultUdpTransportMapping();
-
-transport.listen();
-
-CommunityTarget comtarget = new CommunityTarget();
-
-comtarget.setCommunity(community1);
-
-comtarget.setVersion(SnmpConstants.version1);
-
-comtarget.setAddress(targetaddress);
-
-comtarget.setRetries(2);
-
-comtarget.setTimeout(5000);
-
-PDU pdu = new PDU();
-
-ResponseEvent response;
-
-Snmp snmp;
-
-pdu.add(new VariableBinding(new OID(strOID)));
-pdu.add(new VariableBinding(new OID(strOID2)));
-pdu.add(new VariableBinding(new OID(strOID3)));
-
-pdu.setType(PDU.GET);
-
-snmp = new Snmp(transport);
-
-response = snmp.get(pdu, comtarget);
-
-if(response != null)
-
-{
-
-if(response.getResponse().getErrorStatusText().equalsIgnoreCase("Success"))
-
-{
-
-PDU pduresponse=response.getResponse();
-
-str=pduresponse.getVariableBindings().toString();
-
-if(str.contains(","))
-
-{
-
-int len = str.indexOf(",");
-//System.out.print(str);
-Umid=str.substring(41,43);
-Fum=str.substring(63,64);
-Temp=str.substring(19,21);
-
 }
-
-}
-
-}
-
-else
-
-{
-
-System.out.println("Erro: TimeOut");
-
-}
-
-snmp.close();
-
-} catch(Exception e) { e.printStackTrace(); }
-
-return ("Temp= "+Temp+" Umid= "+ Umid + " Fum= " +Fum);
-
-}
-
 }
---
+++ Cliente_SNMP/src/clientesnmp/Req_SNMP.java
@@ -0,0 +1,125 @@
+package clientesnmp;
+
+import java.util.regex.Pattern;
+
+
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.PDU;
+import org.snmp4j.Snmp;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.Address;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.UdpAddress;
+import org.snmp4j.smi.VariableBinding;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+
+public class Req_SNMP {
+	public String snmpGet(String strAddress, String community, String strOID, String strOID2, String strOID3)
+
+	{
+		
+	String str = "";
+	String Temp="";
+	String Umid = "";
+	String Fum = "";
+	String Fum2 = "";
+	int i =0;
+	try
+	{
+
+	OctetString community1 = new OctetString(community); //comunidade
+
+	strAddress= strAddress+"/" + 161; // ip destino
+
+	Address targetaddress = new UdpAddress(strAddress); 
+
+	TransportMapping transport = new DefaultUdpTransportMapping();
+
+	transport.listen();
+
+	CommunityTarget comtarget = new CommunityTarget();
+
+	comtarget.setCommunity(community1);
+
+	comtarget.setVersion(SnmpConstants.version1);
+
+	comtarget.setAddress(targetaddress);
+
+	comtarget.setRetries(2);
+
+	comtarget.setTimeout(5000);
+
+	PDU pdu = new PDU();
+
+	ResponseEvent response;
+
+	Snmp snmp;
+
+	pdu.add(new VariableBinding(new OID(strOID)));
+	pdu.add(new VariableBinding(new OID(strOID2)));
+	pdu.add(new VariableBinding(new OID(strOID3)));
+
+	pdu.setType(PDU.GET);
+
+	snmp = new Snmp(transport);
+
+	response = snmp.get(pdu, comtarget);
+
+	if(response != null)
+
+	{
+
+	if(response.getResponse().getErrorStatusText().equalsIgnoreCase("Success"))
+
+	{
+
+	PDU pduresponse=response.getResponse();
+
+	str=pduresponse.getVariableBindings().toString();
+
+	if(str.contains(","))
+
+	{
+	i=0;
+	while(i<3){
+		String[] resultado = str.split(",");
+		String[] resultado2 = resultado[i].split("=");
+		System.out.println(resultado2[1]);
+		//int len = str.indexOf(",");
+		//System.out.print(str);
+		if(i==0) {
+			Temp=resultado2[1].replaceAll("\\D", "");
+		}else if (i==1) {
+			Umid=resultado2[1].replaceAll("\\D", "");
+		}else if (i==2) {
+			Fum=resultado2[1].replaceAll("\\D", "");
+		}
+		
+		i++;
+	}
+
+	}
+
+	}
+
+	}
+
+	else
+
+	{
+
+	System.out.println("Erro: TimeOut");
+
+	}
+
+	snmp.close();
+
+	} catch(Exception e) { e.printStackTrace(); }
+
+	return ("Temp= "+Temp+" Umid= "+ Umid + " Fum= " + Fum);
+	//return ("test");
+	}
+}
